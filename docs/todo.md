3d8e851fae87b7b85f62f87b5c3d2a67def77a72
Smerth <merth@me.com>
2017-01-09 21:54:58 -0800

    Main menu overhaul - swapped accordion menu in for drill down - js to add active class - js to toggle accordion - added todo - fixed style issues

---
+++ docs/todo.md
@@ -0,0 +1,10 @@
+# Foundation 6 Sandbox ToDo
+
+- [ ] Reorganize the main menu
+- [ ] Fix the links to main menu item (if filename is available to panini, print it in each page...)
+- [ ] Add a standardized info box and icon to each page to explain what is being demoed
+- [ ] Make sure functionality being demoed actually works with a decent set of examples.
+- [ ] See what components you've missed or not implemented.
+- [ ] Fix chinese card duplicates  and make sure data feed is working.
+- [ ] Add a section called integrations to list pages that demo several components or libraries in an interesting way.
+- [ ] Put the chinese character data on dropbox to access it instead of a drupal site.
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,52 @@ $(document).foundation(
 );
 
 // ********************************
+// Add active class to active menu links in the main menu
+// from the comments: 
+// https://css-tricks.com/snippets/jquery/add-active-navigation-class-based-on-url/
+// ********************************
+
+$(document).ready(function() {
+    (function() {
+
+        var nav = document.getElementById('main-menu'),
+            anchor = nav.getElementsByTagName('a'),
+            current = window.location.pathname.split('/')[2];
+
+        for (var i = 0; i < anchor.length; i++) {
+            // depends on fixed url structure
+            if (anchor[i].href.split('/')[4] == current) {
+                anchor[i].className = "active";
+            }
+        }
+    })();
+});
+
+// $(document).ready(function() {
+//     (function() {
+
+//         var newClassName = "is-active",
+//             nav = document.getElementById('main-menu'),
+//             activelink = nav.querySelector("a.active");
+
+//         var target = activelink.parentNode.parentNode;
+//         // $('#main-menu').foundation('toggle', $target);
+//         $('#element').foundation('down', $;
+//     })();
+// });
+
+
+$(document).ready(function() {
+    $('#main-menu a.active').closest('ul').addClass('is-active');
+    var target = $('#main-menu a.active').closest('ul');
+    $('#main-menu').foundation('down', target);
+});
+
+
+
+
+
+// ********************************
 // MAGNIFIC.JS - alternate lightbox plugin
 // ********************************
 
@@ -28,43 +74,43 @@ $(document).ready(function() {
 
 
 
-$('.parent-container').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image'
-        // other options
-});
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image'
+            // other options
+    });
 
 
-$('#single-object').magnificPopup({
-    items: {
-        src: 'http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_b.jpg'
-    },
-    type: 'image' // this is default type
-});
+    $('#single-object').magnificPopup({
+        items: {
+            src: 'http://farm9.staticflickr.com/8225/8558295635_b1c5ce2794_b.jpg'
+        },
+        type: 'image' // this is default type
+    });
 
-$('#multiple-objects').magnificPopup({
-    items: [{
-        src: 'http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_b.jpg'
-    }, {
-        src: 'http://vimeo.com/161890877',
-        type: 'iframe' // this overrides default type
-    }, {
-        src: $('<div>Dynamically created element</div>'), // Dynamically created element
-        type: 'inline'
-    }, {
-        src: '<div>HTML string</div>',
-        type: 'inline'
-    }, {
-        src: '#my-popup', // CSS selector of an element on page that should be used as a popup
-        type: 'inline'
-    }],
-    gallery: {
-        enabled: true
-    },
+    $('#multiple-objects').magnificPopup({
+        items: [{
+            src: 'http://farm9.staticflickr.com/8235/8558295467_e89e95e05a_b.jpg'
+        }, {
+            src: 'http://vimeo.com/161890877',
+            type: 'iframe' // this overrides default type
+        }, {
+            src: $('<div>Dynamically created element</div>'), // Dynamically created element
+            type: 'inline'
+        }, {
+            src: '<div>HTML string</div>',
+            type: 'inline'
+        }, {
+            src: '#my-popup', // CSS selector of an element on page that should be used as a popup
+            type: 'inline'
+        }],
+        gallery: {
+            enabled: true
+        },
 
 
-    type: 'image' // this is default type
-});
+        type: 'image' // this is default type
+    });
 
 
 });
@@ -77,78 +123,76 @@ $('#multiple-objects').magnificPopup({
 // PHOTSWIPE.JS - alternate lightbox plugin made for swiping on mobile devices
 // ********************************
 $(document).ready(function() {
-  // put all your jQuery goodness in here.
-  $('#open-photoswipe').click( function() {
+    // put all your jQuery goodness in here.
+    $('#open-photoswipe').click(function() {
 
         var pswpElement = document.querySelectorAll('.pswp')[0];
 
-// build items array
-var items = [
-    {
-        src: 'https://placekitten.com/600/400',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'https://placekitten.com/1200/900',
-        w: 1200,
-        h: 900
-    }
-];
+        // build items array
+        var items = [{
+                src: 'https://placekitten.com/600/400',
+                w: 600,
+                h: 400
+            },
+            {
+                src: 'https://placekitten.com/1200/900',
+                w: 1200,
+                h: 900
+            }
+        ];
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-};
+        // define options (if needed)
+        var options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
+        // Initializes and opens PhotoSwipe
+        var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
 
 
-    return false;
+        return false;
 
-} );
+    });
 });
 
 
 $(document).ready(function() {
 
-$('#open-photoswipe').click( function() {
+    $('#open-photoswipe').click(function() {
 
         var pswpElement = document.querySelectorAll('.pswp')[0];
 
-// build items array
-var items = [
-    {
-        src: 'https://placekitten.com/600/400',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'https://placekitten.com/1200/900',
-        w: 1200,
-        h: 900
-    }
-];
+        // build items array
+        var items = [{
+                src: 'https://placekitten.com/600/400',
+                w: 600,
+                h: 400
+            },
+            {
+                src: 'https://placekitten.com/1200/900',
+                w: 1200,
+                h: 900
+            }
+        ];
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-};
+        // define options (if needed)
+        var options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
+        // Initializes and opens PhotoSwipe
+        var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
 
 
-    return false;
+        return false;
 
-} );
+    });
 
 
 });
@@ -174,58 +218,58 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 
-// Example 1
-$('.isotope-example-1').isotope({
-    // options
-    itemSelector: '.grid-item',
-    masonry: {
-        columnWidth: 100
-    }
-});
+    // Example 1
+    $('.isotope-example-1').isotope({
+        // options
+        itemSelector: '.grid-item',
+        masonry: {
+            columnWidth: 100
+        }
+    });
 
-// Example 2
-$('.isotope-example-2').isotope({
-    // options
-    itemSelector: '.grid-item',
-    masonry: {
-        columnWidth: 100
-    }
-});
-
-
-
-// Isotope Margins
-// external js: isotope.pkgd.js
+    // Example 2
+    $('.isotope-example-2').isotope({
+        // options
+        itemSelector: '.grid-item',
+        masonry: {
+            columnWidth: 100
+        }
+    });
 
 
-// init Isotope
-var $container = $('.isotope-example-3').isotope({
-    // options
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
-});
 
-var $container = $('.isotope-example-4').isotope({
-    // options
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
-});
+    // Isotope Margins
+    // external js: isotope.pkgd.js
 
 
+    // init Isotope
+    var $container = $('.isotope-example-3').isotope({
+        // options
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
+    });
+
+    var $container = $('.isotope-example-4').isotope({
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
 
 
 
 
 
-// ******* FLUID SIZING PAGE
-// external js: isotope.pkgd.js
-$('.grid-masonry-fluild-sizing').isotope({
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    masonry: {
-        columnWidth: '.grid-sizer'
-    }
-});
+
+
+    // ******* FLUID SIZING PAGE
+    // external js: isotope.pkgd.js
+    $('.grid-masonry-fluild-sizing').isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+            columnWidth: '.grid-sizer'
+        }
+    });
 
 });
 
@@ -268,27 +312,27 @@ $('.grid-masonry-fluild-sizing').isotope({
 
 $(document).ready(function() {
     $(".jquery-flip-card-1").flip({
-    axis: 'x',
-    trigger: 'click',
-    forceWidth: false,
-    forceHeight: true
-});
+        axis: 'x',
+        trigger: 'click',
+        forceWidth: false,
+        forceHeight: true
+    });
 
-$(".jquery-flip-card-2").flip({
-    axis: 'x',
-    trigger: 'click',
-    forceWidth: false,
-    forceHeight: true
-});
-   
+    $(".jquery-flip-card-2").flip({
+        axis: 'x',
+        trigger: 'click',
+        forceWidth: false,
+        forceHeight: true
+    });
 
- 
-$(".jquery-flip-chinese-card").flip({
-    axis: 'y',
-    trigger: 'click',
-    forceWidth: false,
-    forceHeight: false
-});
+
+
+    $(".jquery-flip-chinese-card").flip({
+        axis: 'y',
+        trigger: 'click',
+        forceWidth: false,
+        forceHeight: false
+    });
 });
 
 
@@ -307,37 +351,37 @@ var flip = flippant.flip
 
 
 document.addEventListener('click', function(e) {
-  if (e.target.classList.contains('btnflip')) {
-    e.preventDefault()
-    var flipper = e.target.parentNode.parentNode
-    var back
-    var input = '<p><input type="text" value="' + flipper.querySelector('h2').innerText + '"></p>'
-    var textarea = '<textarea style="width:100%; max-width:32em; height:12em;">' + flipper.querySelector('p').innerText + '</textarea><br><button type="button" class="btn button">Update</button>'
+    if (e.target.classList.contains('btnflip')) {
+        e.preventDefault()
+        var flipper = e.target.parentNode.parentNode
+        var back
+        var input = '<p><input type="text" value="' + flipper.querySelector('h2').innerText + '"></p>'
+        var textarea = '<textarea style="width:100%; max-width:32em; height:12em;">' + flipper.querySelector('p').innerText + '</textarea><br><button type="button" class="btn button">Update</button>'
 
-    if (e.target.classList.contains('card')) {
-      back = flip(flipper, "<p>It's a card!</p>" + input + textarea)
-    } else {
-      back = flip(flipper, "<p>It's a modal!</p>" + input + textarea, 'modal')
+        if (e.target.classList.contains('card')) {
+            back = flip(flipper, "<p>It's a card!</p>" + input + textarea)
+        } else {
+            back = flip(flipper, "<p>It's a modal!</p>" + input + textarea, 'modal')
+        }
+
+        back.addEventListener('click', function(e) {
+            if (e.target.classList.contains('btn')) {
+                flipper.querySelector('h2').innerText = back.querySelector('input').value
+                flipper.querySelector('p').innerText = back.querySelector('textarea').value
+                back.close(back, 'close')
+            }
+        })
+
     }
-
-    back.addEventListener('click', function(e) {
-      if (e.target.classList.contains('btn')) {
-        flipper.querySelector('h2').innerText = back.querySelector('input').value
-        flipper.querySelector('p').innerText = back.querySelector('textarea').value
-        back.close(back, 'close')
-      }
-    })
-
-  }
 })
 
 // THIS ONE WORKS ON FIREFOX
 $(document).ready(function($) {
 
-    $('.flippant-open').click(function (e) {
+    $('.flippant-open').click(function(e) {
         var flip = flippant.flip
 
-        "use strict";
+            "use strict";
         var front_content = e.target.parentNode;
         var card = e.target.parentNode.parentNode;
         var back_content = card.getElementsByTagName('div')[1].innerHTML;
@@ -349,7 +393,7 @@ $(document).ready(function($) {
             back = flip(front_content, back_content, 'modal');
         }
 
-        back.addEventListener('click', function (e) {
+        back.addEventListener('click', function(e) {
             if (e.target.classList.contains('flippant-close')) {
                 back.close(back, 'close');
             }
@@ -366,18 +410,15 @@ $(document).ready(function($) {
     // var flip = flippant.flip;
     // var back = null;
 
-// $(".flippant-open").on("click", function(evt) {
+    // $(".flippant-open").on("click", function(evt) {
     // var card = $(this).parentsUntil(article).html();
     // var card = $(this).parent().parent().html();
     // var front_content = $(this).parent().html();
     // var card = $(this).parentsUntil("article").html(); // Can't figure out how to ues this
-    
+
     // alert(card);
 
-// });
+    // });
 
 
 });
-
-
-
